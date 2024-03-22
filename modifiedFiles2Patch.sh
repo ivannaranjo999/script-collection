5cb3b2b510236e17f34509ad875df8f308f37056
Ivan Naranjo Ortega <iino@gmv.com>
2024-03-22 07:44:20 +0000

    modifiedFiles2Patch.sh

---
+++ modifiedFiles2Patch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+	echo "2 arguments necessary"
+	echo ""
+	echo -e "\t- Path to rtmc directory"
+	echo -e "\t- Path to patch file"
+	exit 1
+fi
+
+cd $1
+files=$(git diff --name-only master HEAD)
+cd - > /dev/null
+cd $2
+
+for fn in $files; do
+	mkdir -p $(dirname $fn)
+	cp ../SPR1373_branch/rtmc/$fn $(dirname $fn)
+	echo "$(basename $fn) copied"	
+done
+
+echo "Exiting..."
+exit 0
